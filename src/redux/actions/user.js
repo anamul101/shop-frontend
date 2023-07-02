98088d24861deb2125a242125e54a94086902380
anamul101 <rjanamul01871@gmail.com>
2023-07-02 11:47:11 +0100

    oe

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { server } from "../../server";
-
 // load user
 export const loadUser = () => async(dispatch) => {
     try {
         dispatch({
             type:  "LoadUserRequest",
         });
-        const {data} = await axios.get(`${server}/user/getuser`, {withCredentials:true});
+        const {data} = await axios.get(`${server}/user/getuser`);
+        console.log(data)
         dispatch({
             type: "LoadUserSuccess",
             payload: data.user,
